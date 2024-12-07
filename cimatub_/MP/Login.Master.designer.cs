ca3ed1bf1de20c684e6b1245da5ad0be1a293493
Alejandro Velazquez <alejandrovelazquezmercado21@gmail.com>
2024-12-07 02:30:57 -0800

    avance

--- cimatub_/MP/Login.Master.designer.cs
+++ cimatub_/MP/Login.Master.designer.cs
@@ -22,5 +22,23 @@ namespace cimatub_.MP
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// Control form132.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form132;
+
+        /// <summary>
+        /// Control MainContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
--- cimatub_/Pantallas/Inicio.aspx.cs
+++ cimatub_/Pantallas/Inicio.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,9 +12,41 @@ namespace cimatub_.Pantallas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (!IsPostBack)
+            {
+                // Cargar datos de prueba en los ListView
+                ListViewDestacados.DataSource = GetPlaceholderData(4); // 4 videos destacados
+                ListViewDestacados.DataBind();
+
+                ListViewRecomendados.DataSource = GetPlaceholderData(10); // 10 videos recomendados
+                ListViewRecomendados.DataBind();
+            }
         }
 
+        // Método para generar datos de prueba
+        private DataTable GetPlaceholderData(int count)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("VideoId");
+            table.Columns.Add("ThumbnailUrl");
+            table.Columns.Add("Title");
+            table.Columns.Add("Channel");
+
+            for (int i = 1; i <= count; i++)
+            {
+                table.Rows.Add(
+                    i, // VideoId
+                    "https://via.placeholder.com/320x180.png?text=Video+" + i, // ThumbnailUrl
+                    "Título del Video " + i,                                  // Title
+                    "Canal " + i                                              // Channel
+                );
+            }
+
+            return table;
+        }
+
+
+
     }
-    
+
 }
